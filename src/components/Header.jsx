0828c63b29043e69214ccd6ec3baa65de311ba8e
Krishna080492 <krishna.desai4@gmail.com>
2024-10-28 19:27:51 +0530

    cart-count-number done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import "../App.css";
 
 function Header() {
+  let cartCount = useSelector((state) => state.product.cart.length);
+
   return (
     <>
       {/* Start Header/Navigation */}
       <nav
-        className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark"
+        className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark "
         arial-label="Furni navigation bar"
       >
         <div className="container">
@@ -66,6 +70,9 @@ function Header() {
               <li>
                 <NavLink className="nav-link" to="/cart">
                   <img src="images/cart.svg" />
+                  {cartCount > 0 && (
+                    <span className="cart-count">{cartCount}</span>
+                  )}
                 </NavLink>
               </li>
             </ul>
